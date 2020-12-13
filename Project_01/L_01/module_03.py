e5f0b2bc0abd9ee180fcfe558122106a89e39ec8
Vlad <mirror.book@yandex.ru>
2020-12-13 22:40:19 +0300

    Практические задания после первого урока.

---
+++ Project_01/L_01/module_03.py
@@ -0,0 +1,12 @@
+
+n = str
+nn = str
+nnn = str
+nnnnnn = int
+
+print("Урок 1. Пункт 3")
+n = input("Введите, пожалуйста, целое положительное число: ")
+nn = n + n
+nnn = n + n + n
+nnnnnn = int(n) + int(nn) + int(nnn)
+print(f"Тогда: {n} + {nn} + {nnn} = {nnnnnn}")
